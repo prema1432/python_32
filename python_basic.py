9a08a993d9d027c3465dd3459268673ad8831bd6
Premanath Talamarla <premanath@syoft.com>
2024-06-14 21:18:54 +0530

    first class

---
+++ python_basic.py
@@ -0,0 +1,77 @@
+# 1. python Basics
+# - Comments
+
+
+# 1.Identifiers
+
+# Varobale,function names, clas names , -
+
+a = "234"
+
+print(f"My out put : {a} ")
+
+# Rules
+
+# 1. Can not start with number
+# 3name ="mahesh"
+# print("Mahesh name",3name)
+
+# 2. Can not start with special characters
+
+hello = "hello world"
+print("Hello world", hello)
+
+# hello@ = "hello world4"
+# print("Hello world",hello@)
+
+# @hello = "hello world"
+# print("Hello world",@hello)
+
+# 3. Can not contain spaces
+
+# hello = "hello world"
+# print("Hello world",hello)
+
+
+welcome = "hello world"
+print("Small", welcome)
+
+WELCOME = "hello world welcome"
+print("Capital ", WELCOME)
+
+welcome = "python hello world"
+print("Small", welcome)
+
+import keyword
+
+print("Reserved Keywords : ", keyword.kwlist)
+
+# ('False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break',
+#  'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for',
+#  'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or',
+#  'pass', 'raise', 'return', 'try', 'while', 'with', 'yield')
+
+# True, False,None , pass, raise, return
+# and , not or  as , asseert, async, await, break,
+# class , functyion - def  continue,
+# del -
+# if elif else
+# try exception finally
+# import in is lambda
+# for while
+# with yield
+
+
+# __hg == Its - Strong Private
+# _hjkh == ITs Private Varibale
+# Examples:
+# 1.52364532hjghjg  == Invalid
+# 2.myname23423 === valid
+# 3.capital@ == Invalid
+# 4.#capityla == Invalid
+# 5.__abbbbb__ == valid
+# 6._kio_op === Valid
+# 7.def
+# 8.if
+# global ="hello"
+# print("Global variables",global)
