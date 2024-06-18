cc6612e4bf4447c6ce7e0822709712b61f1254e0
Premanath Talamarla <premanath@syoft.com>
2024-06-18 21:06:43 +0530

    data types 1

---
+++ data_types.py
@@ -0,0 +1,37 @@
+# today we are disccuing the DATA TYPES in Python.
+
+# Data types
+# 1. int - whole number 0 - 999999999999 , Ex: 9999999999999
+
+
+# 2. float - deciamls formats, Ex: 999999999.333333
+
+# 3. complex
+
+# a+bj
+
+
+# 4. bool
+# 5. str
+# 6. list
+# 7. tuple
+# 8. dict
+# 9. set
+# 10. frozenset
+# 11. bytes
+# 12. bytearray
+# 13. NoneType
+
+a = 100.50
+# print("my name is " + a)
+print(type(a)) # data tyep - which data type
+# a = 3
+# print(a)
+# print(id(a)) # id of the object variable
+# b = 44
+# print(b)
+# print(id(b))
+#
+# A = 77
+# print(A)
+# print(id(A))
